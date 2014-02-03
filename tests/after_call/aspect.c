78767f988709b91b735d5f05d1555eed8268780c
Traian Florin Serbanuta <traian.serbanuta@gmail.com>
2014-02-03 02:11:26 -0800

    added tests for the remaining cutpoints

--- tests/after_call/aspect.c
+++ tests/after_call/aspect.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void incAspect(int ret, int arg) {
-  printf("Function inc was called with argument %d and returned %d\n",
-      arg, ret);
+  if (ret>10) {
+    printf("Function inc was called with argument %d and returned %d\n",
+        arg, ret);
+    printf("Maximum limit passed.  Exiting...\n");
+    exit(1);
+  }
 }
